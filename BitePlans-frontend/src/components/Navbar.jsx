d7e5f3d96a7e459199b89baefcbef55cf6f377b0
Ayush Jaiswal <ayushrvscse@gmail.com>
2025-07-15 20:04:34 +0530

    updated

--- BitePlans-frontend/src/components/Navbar.jsx
+++ BitePlans-frontend/src/components/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = () => {
   <img
     src="/BitePlans.png"
     alt="BitePlans Logo"
-    className="h-8 w-8 sm:h-9 sm:w-9 object-contain"
+    className="h-10 w-10 sm:h-12 sm:w-12 object-contain"
   />
   <span className="hidden md:inline text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
     BitePlans
